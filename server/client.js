ab8a797bf864a1feffd8f7ed9239fcffdd299b4f
Adam <adam.kjall@hotmail.com>
2020-02-12 02:06:49 +0100

    refactored servers main.js file and added client.js and session.js

---
+++ server/client.js
@@ -0,0 +1,8 @@
+class Client {
+  constructor(conn) {
+    this.conn = conn;
+    this.session = null;
+  }
+}
+
+module.exports = Client;
--- server/main.js
+++ server/main.js
@@ -1,38 +1,13 @@
 const WebSocketServer = require("ws").Server;
 
+const Session = require("./session");
+const Client = require("./client");
+
 const server = new WebSocketServer({ port: 9000 });
 
 const sessions = new Map();
 
-class Session {
-  constructor(id) {
-    this.id = id;
-    this.clients = new Set();
-  }
 
-  join(client) {
-    if (client.session) {
-      throw new Error("Client already in session");
-    }
-    this.clients.add(client);
-    client.session = this;
-  }
-
-  leave(client) {
-    if (client.session !== this) {
-      throw new Error("Client not in session");
-    }
-    this.clients.delete(client);
-    client.session = null;
-  }
-}
-
-class Client {
-  constructor(conn) {
-    this.conn = conn;
-    this.session = null;
-  }
-}
 
 function createId(len = 6, chars = "abcdefghjkmnopqrstwxyz0123456789") {
   let id = "";
