291645c533046c9ac8d749a6e09368658f7912c9
Adam <adam.kjall@hotmail.com>
2020-02-13 08:25:44 +0100

    update of server code

--- server/client.js
+++ server/client.js
@@ -1,7 +1,33 @@
 class Client {
-  constructor(conn) {
+  constructor(conn, id) {
     this.conn = conn;
+    this.id = id;
     this.session = null;
+  }
+
+  broadcast(data) {
+    if (!this.session) {
+      throw new Error("Can not broadcast without session");
+    }
+
+    data.clientId = this.id;
+
+    this.session.clients.forEach(client => {
+      if (this === client) {
+        return;
+      }
+      client.send(data)
+    })
+  }
+
+  send(data) {
+    const msg = JSON.stringify(data);
+    // console.log(`Sending message: ${msg}`);
+    this.conn.send(msg, err => {
+      if (err) {
+        console.log("Message failed", msg, err);
+      }
+    });
   }
 }
 
--- server/main.js
+++ server/main.js
@@ -7,29 +7,70 @@ const server = new WebSocketServer({ port: 9000 });
 
 const sessions = new Map();
 
-
-
 function createId(len = 6, chars = "abcdefghjkmnopqrstwxyz0123456789") {
   let id = "";
   while (len--) {
-    id += chars[Math.floor(Math.random() * chars.length)]
+    id += chars[Math.floor(Math.random() * chars.length)];
   }
   return id;
 }
 
+function createClient(conn, id = createId()) {
+  return new Client(conn, id);
+}
+
+function createSession(id = createId()) {
+  if (sessions.has(id)) {
+    throw new Error(`Session ${id} already exists`);
+  }
+
+  const session = new Session(id);
+  console.log("Createing session ", session);
+
+  sessions.set(id, session);
+
+  return session;
+}
+
+function getSession(id) {
+  return sessions.get(id);
+}
+
+function broadCastSession(session) {
+  if (!session) return;
+
+  const clients = [...session.clients];
+  clients.forEach(client => {
+    client.send({
+      type: "session-broadcast",
+      peers: {
+        you: client.id,
+        clients: clients.map(client => client.id)
+      }
+    });
+  });
+}
+
 server.on("connection", conn => {
   console.log("Connection established");
-  const client = new Client(conn); 
+  const client = createClient(conn);
 
   conn.on("message", msg => {
-    console.log("Message recieved", msg);
+    const data = JSON.parse(msg);
 
-    if (msg === "create-session") {
-      const id = createId();
-      const session = new Session(id);
+    if (data.type === "create-session") {
+      const session = createSession();
       session.join(client);
-      sessions.set(session.id, session);
-      console.log(sessions);
+      client.send({
+        type: "session-created",
+        id: session.id
+      });
+    } else if (data.type === "join-session") {
+      const session = getSession(data.id) || createSession(data.id);
+      session.join(client);
+      broadCastSession(session);
+    } else if (data.type === "state-update") {
+      client.broadcast(data);
     }
   });
 
@@ -39,8 +80,9 @@ server.on("connection", conn => {
     if (session) {
       session.leave(client);
       if (session.clients.size === 0) {
-        sessions.delete(session.id)
+        sessions.delete(session.id);
       }
     }
+    broadCastSession(session);
   });
 });
